6f3cc0562b7383bebc9929bdb4934dbf8c8bb931
MrAntares <MrAntares@users.noreply.github.com>
2018-03-22 21:22:51 +0100

    Update Entity.js

--- src/Engine/MapEngine/Entity.js
+++ src/Engine/MapEngine/Entity.js
@@ -898,12 +898,8 @@ define(function( require )
 
 			// Show cart (in future)
 			case StatusConst.ON_PUSH_CART:
-        		entity.hasCart = pkt.state;
-				if([0, 23, 4045, 4190, 4191].includes(entity.job)) {
-					entity.CartNum = 99;
-				} else {
-					entity.CartNum = pkt.val[0];
-				}
+        			entity.hasCart = pkt.state;
+				entity.CartNum = pkt.val[0];
 				break;
 
 			// Cast a skill, TODO: add progressbar in shortcut
