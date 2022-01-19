268d34794fa5bcc097796e1db63ee5dd48921d90
MrAntares <MrAntares@users.noreply.github.com>
2022-01-19 15:33:46 +0100

    Added initial sizing

--- src/Renderer/Effects/Songs.js
+++ src/Renderer/Effects/Songs.js
@@ -22,49 +22,49 @@ define(["exports", "Renderer/Effects/Tiles", "Renderer/Effects/FlatColorTile"], 
     g: 0xff/255,
     b: 0xff/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_w.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_w.bmp', 0.5)];
 
  const LullabyEffects = exports.LullabyEffects = [(0, _FlatColorTile2.default)('lullaby', {
     r: 0xed/255,
     g: 0x9e/255,
     b: 0xff/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/zz.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/zz.bmp', 0.5)];
   
  const MrKimEffects = exports.MrKimEffects = [(0, _FlatColorTile2.default)('mrkim', {
     r: 0xfc/255,
     g: 0xc7/255,
     b: 0xc7/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/pocket.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/pocket.bmp', 0.5)];
   
  const EtChaosEffects = exports.EtChaosEffects = [(0, _FlatColorTile2.default)('etchaos', {
     r: 0x80/255,
     g: 0xff/255,
     b: 0xc2/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_g.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_g.bmp', 0.5)];
     
  const DrumEffects = exports.DrumEffects = [(0, _FlatColorTile2.default)('drum', {
     r: 0xed/255,
     g: 0x65/255,
     b: 0xfc/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/melody_b.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/melody_b.bmp', 0.5)];
 
  const NibelungEffects = exports.NibelungEffects = [(0, _FlatColorTile2.default)('nibelung', {
     r: 0x1c/255,
     g: 0xec/255,
     b: 0xff/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/twirl.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/twirl.bmp', 0.5)];
 
  const LokiEffects = exports.LokiEffects = [(0, _FlatColorTile2.default)('loki', {
     r: 0xdc/255,
     g: 0x65/255,
     b: 0xfc/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/safeline.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/safeline.bmp', 0.5)];
 
  //needs to be red yellow blue random
  const AbyssEffects = exports.AbyssEffects = [(0, _FlatColorTile2.default)('abyss', {
@@ -72,28 +72,28 @@ define(["exports", "Renderer/Effects/Tiles", "Renderer/Effects/FlatColorTile"], 
     g: 0xff/255,
     b: 0xff/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/stoneorb.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/stoneorb.bmp', 0.5)];
 
  const SiegfiedEffects = exports.SiegfiedEffects = [(0, _FlatColorTile2.default)('siegfried', {
     r: 0x48/255,
     g: 0x3b/255,
     b: 0xff/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_b.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_b.bmp', 0.5)];
 
  const WhistleEffects = exports.WhistleEffects = [(0, _FlatColorTile2.default)('whislte', {
     r: 0xff/255,
     g: 0xc0/255,
     b: 0xcb/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/melody_b.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/melody_b.bmp', 0.5)];
 
   const SinEffects = exports.SinEffects = [(0, _FlatColorTile2.default)('sinsun', {
     r: 0xff/255,
     g: 0.8,
     b: 0.85,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_r.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_r.bmp', 0.5)];
  
   exports.BragiEffects = function BragiEffects(){
 	  return [(0, _FlatColorTile2.default)('bragi', {
@@ -101,7 +101,7 @@ define(["exports", "Renderer/Effects/Tiles", "Renderer/Effects/FlatColorTile"], 
 		g: 0.9,
 		b: 1,
 		a: 0.05
-	  }), (0, _Tiles.HoveringTexture)('data/texture/effect/spell_0'+( Math.floor(Math.random()*7) + 1 )+'.bmp', 0.5)];
+	  }), (0, _Tiles.HoveringTexture)('data/texture/effect/spell_0'+( Math.floor(Math.random()*7) + 1 )+'.bmp', 0.5, 0.5)];
   };
 
   const AppleEffects = exports.AppleEffects = [(0, _FlatColorTile2.default)('apple', {
@@ -109,70 +109,70 @@ define(["exports", "Renderer/Effects/Tiles", "Renderer/Effects/FlatColorTile"], 
     g: 0xff/255,
     b: 0x00/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/idun_apple.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/idun_apple.bmp', 0.5)];
 
   const UglyEffects = exports.UglyEffects = [(0, _FlatColorTile2.default)('ugly', {
     r: 0xff/255,
     g: 0xff/255,
     b: 0xff/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_w.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_w.bmp', 0.5)];
 
   const HummingEffects = exports.HummingEffects = [(0, _FlatColorTile2.default)('humming', {
     r: 0xe6/255,
     g: 0xd1/255,
     b: 0xd1/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/melody_a.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/melody_a.bmp', 0.5)];
 
   const ForgetEffects = exports.ForgetEffects = [(0, _FlatColorTile2.default)('humming', {
     r: 0x1c/255,
     g: 0xff/255,
     b: 0x73/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_g.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_g.bmp', 0.5)];
 
   const FortuneEffects = exports.FortuneEffects = [(0, _FlatColorTile2.default)('fortune', {
     r: 0xfc/255,
     g: 0x6f/255,
     b: 0x65/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/heart_1.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/heart_1.bmp', 0.5)];
 
   const ServiceEffects = exports.ServiceEffects = [(0, _FlatColorTile2.default)('service', {
     r: 0xff/255,
     g: 0x80/255,
     b: 0xb7/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/safeline.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/safeline.bmp', 0.5)];
 
   const GospelEffects = exports.GospelEffects = [(0, _FlatColorTile2.default)('gospel', {
     r: 0xff/255,
     g: 0xff/255,
     b: 0xff/255,
     a: 0.05
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/cross_old.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/cross_old.bmp', 0.5)];
 
   const FogEffects = exports.FogEffects = [(0, _FlatColorTile2.default)('fog', {
     r: 0xaa/255,
     g: 0xaa/255,
     b: 0xaa/255,
     a: 0.6
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_w.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_w.bmp', 0.5)];
 
   const GravityEffects = exports.GravityEffects = [(0, _FlatColorTile2.default)('gravity', {
     r: 0xff/255,
     g: 0xff/255,
     b: 0xff/255,
     a: 0.2
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_w.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/lens_w.bmp', 0.5)];
 
   const EvillandEffects = exports.EvillandEffects = [(0, _FlatColorTile2.default)('gray', {
     r: 0xa0/255,
     g: 0xa0/255,
     b: 0xa0/255,
     a: 0.2
-  }), (0, _Tiles.HoveringTexture)('data/texture/effect/curse.bmp')];
+  }), (0, _Tiles.HoveringTexture)('data/texture/effect/curse.bmp', 1)];
   
  
 });
