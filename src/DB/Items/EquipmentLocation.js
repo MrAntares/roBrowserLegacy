51378c285da3b0943594ee696c7bbc43758f1542
MrAntares <MrAntares@users.noreply.github.com>
2022-02-11 11:59:14 +0100

    Update EquipmentLocation.js

--- src/DB/Items/EquipmentLocation.js
+++ src/DB/Items/EquipmentLocation.js
@@ -24,6 +24,17 @@ define(function()
 		ACCESSORY2:  1 << 7,
 		HEAD_TOP:    1 << 8,
 		HEAD_MID:    1 << 9,
-		AMMO:        1 << 15
+		COSTUME_HEAD_TOP:    1 << 10,
+		COSTUME_HEAD_MID:    1 << 11,
+		COSTUME_HEAD_BOTTOM: 1 << 12,
+		COSTUME_ROBE:        1 << 13,
+		COSTUME_FLOOR:       1 << 14,
+		AMMO:                1 << 15,
+		SHADOW_ARMOR:        1 << 16,
+		SHADOW_WEAPON:       1 << 17,
+		SHADOW_SHIELD:       1 << 18,
+		SHADOW_SHOES:        1 << 19,
+		SHADOW_R_ACCESSORY_SHADOW: 1 << 20,
+		SHADOW_L_ACCESSORY_SHADOW: 1 << 21
 	};
 });
