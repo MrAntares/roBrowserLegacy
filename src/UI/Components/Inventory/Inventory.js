452f8a67f3516d16e834b69b85e84f1004a69891
Antares <MrAntares@users.noreply.github.com>
2022-03-01 14:12:39 +0100

    Default item count

--- src/UI/Components/Inventory/Inventory.js
+++ src/UI/Components/Inventory/Inventory.js
@@ -109,6 +109,7 @@ define(function(require)
 				.on('contextmenu', '.item', onItemInfo)
 				.on('dblclick',    '.item', onItemUsed);
 			
+		this.ui.find('.ncnt').text(100);
 		this.ui.find('.mcnt').text(100);
 
 		this.draggable(this.ui.find('.titlebar'));
