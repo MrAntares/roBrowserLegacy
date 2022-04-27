e1a6870f2013fa2ac1a990784285ecd0f428ac16
Antares <MrAntares@users.noreply.github.com>
2022-04-27 22:02:59 +0200

    Camera overhaul
    Added the option for 1st & 3rd person view.
    By default it is disabled. To enable it set the following to true:
    Line 132: `Camera.enable3RDPerson = false;`
    Line 133: `Camera.enable1STPerson = false;`

--- src/Renderer/Camera.js
+++ src/Renderer/Camera.js
@@ -24,8 +24,23 @@ define(function( require )
 	var vec2        = glMatrix.vec2;
 	var vec3        = glMatrix.vec3;
 	var _position   = vec3.create();
-
-
+	
+	/**
+	 * @var {number} camera min-max constants
+	 */
+	const C_MIN_ZOOM = 1;
+	const C_MAX_ZOOM = 5;
+	
+	const C_MIN_V_ANGLE_ISOMORPHIC = 190;
+	const C_MAX_V_ANGLE_ISOMORPHIC = 270;
+	
+	const C_THIRDPERSON_TRESHOLD_ZOOM = 1;
+	const C_MIN_V_ANGLE_3RDPERSON = 175;
+	const C_MAX_V_ANGLE_3RDPERSON = 270;
+	
+	const C_MIN_V_ANGLE_1STPERSON = 90;
+	const C_MAX_V_ANGLE_1STPERSON = 270;
+	
 	/**
 	 * Camera Namespace
 	 */
@@ -93,23 +108,37 @@ define(function( require )
 	 */
 	Camera.lastTick = 0;
 
-
 	/**
-	 * @var {number} camera zoom
+	 * @var {number} camera min-max variables
 	 */
-	Camera.MAX_ZOOM = 20;
-
+	Camera.MIN_ZOOM = C_MIN_ZOOM;
+	Camera.MAX_ZOOM = C_MAX_ZOOM;
+	Camera.MIN_V_ANGLE = C_MIN_V_ANGLE_ISOMORPHIC;
+	Camera.MAX_V_ANGLE = C_MAX_V_ANGLE_ISOMORPHIC;
 
 	/**
 	 * @var {number} Camera direction
 	 */
 	Camera.direction    =    0;
-	Camera.altitudeFrom =  -50;
+	Camera.altitudeFrom =    0;
 	Camera.altitudeTo   =  -65;
+	Camera.altitudeRange = 15;
 	Camera.rotationFrom = -360;
 	Camera.rotationTo   =  360;
 	Camera.range        =  230; //240;
-
+	Camera.zoomStep     =  15;
+	Camera.zoomStepMult =  1;
+	
+	Camera.enable3RDPerson = false;
+	Camera.enable1STPerson = false;
+	
+	Camera.state = -1;
+	
+	Camera.states = {
+		isomorphic: 0,
+		third_person: 1,
+		first_person: 2
+	};
 
 	/**
 	 * @var {object} Camera action informations (right click)
@@ -159,6 +188,18 @@ define(function( require )
 		this.position[0] = -this.target.position[0];
 		this.position[1] = -this.target.position[1];
 		this.position[2] =  this.target.position[2];
+		
+		this.altitudeRange = this.altitudeTo - this.altitudeFrom;
+		
+		if(this.enable1STPerson){
+			this.MIN_ZOOM = 0;
+		} else if(this.enable3RDPerson){
+			this.MIN_ZOOM = 0.2;
+		} else {
+			this.MIN_ZOOM = C_MIN_ZOOM;
+		}
+		
+		//this.updateState();
 	};
 
 
@@ -232,15 +273,15 @@ define(function( require )
 		// Rotate Z
 		if (KEYS.SHIFT) {
 			this.angleFinal[0] += ( Mouse.screen.y - this.action.y ) / Mouse.screen.height * 300;
-			this.angleFinal[0]  = Math.max( this.angleFinal[0], 190 );
-			this.angleFinal[0]  = Math.min( this.angleFinal[0], 270 );
+			this.angleFinal[0]  = Math.max( this.angleFinal[0], this.MIN_V_ANGLE );
+			this.angleFinal[0]  = Math.min( this.angleFinal[0], this.MAX_V_ANGLE );
 		}
 
 		// Zoom
 		else if (KEYS.CTRL) {
-			this.zoomFinal -= ( Mouse.screen.y - this.action.y  ) / Mouse.screen.height * 150;
-			this.zoomFinal  = Math.min( this.zoomFinal, Math.abs(this.altitudeTo-this.altitudeFrom) * this.MAX_ZOOM );
-			this.zoomFinal  = Math.max( this.zoomFinal,  2.0 );
+			this.zoomFinal -= ( Mouse.screen.y - this.action.y  ) / (this.zoomStep * 10);
+			this.zoomFinal  = Math.min( this.zoomFinal, Math.abs(this.altitudeRange) * this.MAX_ZOOM );
+			this.zoomFinal  = Math.max( this.zoomFinal, Math.abs(this.altitudeRange) * this.MIN_ZOOM );
 		}
 
 		// Rotate
@@ -259,12 +300,18 @@ define(function( require )
 
 			this.angleFinal[1] = Math.max( this.angleFinal[1], this.rotationFrom );
 			this.angleFinal[1] = Math.min( this.angleFinal[1], this.rotationTo );
+			
+			if(this.state == this.states.first_person || this.state == this.states.third_person){
+				this.angleFinal[0] += ( Mouse.screen.y - this.action.y ) / Mouse.screen.height * 300;
+				this.angleFinal[0]  = Math.max( this.angleFinal[0], this.MIN_V_ANGLE );
+				this.angleFinal[0]  = Math.min( this.angleFinal[0], this.MAX_V_ANGLE );
+			}
 		}
 
 		// Update last check
 		this.action.x = +Mouse.screen.x ;
 		this.action.y = +Mouse.screen.y ;
-
+		this.updateState();
 		this.save();
 	};
 
@@ -277,13 +324,47 @@ define(function( require )
 	Camera.setZoom = function SetZoom( delta )
 	{
 		if(delta){
-			this.zoomFinal += delta * 15;
-			this.zoomFinal  = Math.min( this.zoomFinal, Math.abs(this.altitudeTo-this.altitudeFrom) * this.MAX_ZOOM );
-			this.zoomFinal  = Math.max( this.zoomFinal,  2.0 );
-
+			this.zoomFinal += delta * this.zoomStep * this.zoomStepMult;
+			this.zoomFinal  = Math.min( this.zoomFinal, Math.abs(this.altitudeRange) * this.MAX_ZOOM );
+			this.zoomFinal  = Math.max( this.zoomFinal, Math.abs(this.altitudeRange) * this.MIN_ZOOM );
+			this.updateState();
 			this.save();
 		}
 	};
+	
+	Camera.updateState = function UpdateState(){
+		if(this.enable1STPerson && this.zoomFinal == 0){
+			if(this.state != this.states.first_person){
+				var Renderer    = require('Renderer/Renderer');
+				this.MIN_V_ANGLE = C_MIN_V_ANGLE_1STPERSON;
+				this.MAX_V_ANGLE = C_MAX_V_ANGLE_1STPERSON;
+				Renderer.vFov = 50;
+				Renderer.resize();
+				this.zoomStepMult = 0.3;
+				this.state = this.states.first_person;
+			}
+		} else if (this.enable3RDPerson &&  this.zoomFinal < (Math.abs(this.altitudeRange) * C_THIRDPERSON_TRESHOLD_ZOOM)){
+			if(this.state != this.states.third_person){
+				var Renderer    = require('Renderer/Renderer');
+				this.MIN_V_ANGLE = C_MIN_V_ANGLE_3RDPERSON;
+				this.MAX_V_ANGLE = C_MAX_V_ANGLE_3RDPERSON;
+				Renderer.vFov = 30;
+				Renderer.resize();
+				this.zoomStepMult = 0.3;
+				this.state = this.states.third_person;
+			}
+		} else {
+			if(this.state != this.states.isomorphic){
+				var Renderer    = require('Renderer/Renderer');
+				this.MIN_V_ANGLE = C_MIN_V_ANGLE_ISOMORPHIC;
+				this.MAX_V_ANGLE = C_MAX_V_ANGLE_ISOMORPHIC;
+				Renderer.vFov = 15;
+				Renderer.resize();
+				this.zoomStepMult = 1;
+				this.state = this.states.isomorphic;
+			}
+		}
+	}
 
 
 	/**
@@ -293,6 +374,7 @@ define(function( require )
 	 */
 	Camera.update = function Update( tick )
 	{
+		
 		var lerp      = Math.min( (tick - this.lastTick) * 0.006, 1.0);
 		this.lastTick = tick;
 
@@ -300,9 +382,9 @@ define(function( require )
 		if (this.action.x !== -1 && this.action.y !== -1 && this.action.active) {
 			this.processMouseAction();
 		}
-
+		
 		// Move Camera
-		if (Preferences.smooth) {
+		if (Preferences.smooth && this.state != this.states.first_person) {
 			this.position[0] += ( -this.target.position[0] - this.position[0] ) * lerp ;
 			this.position[1] += ( -this.target.position[1] - this.position[1] ) * lerp ;
 			this.position[2] += (  this.target.position[2] - this.position[2] ) * lerp ;
@@ -312,29 +394,36 @@ define(function( require )
 			this.position[1] = -this.target.position[1];
 			this.position[2] =  this.target.position[2];
 		}
-
+		
 		// Zoom
 		this.zoom        += ( this.zoomFinal - this.zoom ) * lerp * 2.0;
-
+		
+		var zOffset = 0;
+		if(this.state == this.states.first_person){
+			zOffset = 2;
+		} else if (this.state == this.states.third_person && this.zoomFinal < (Math.abs(this.altitudeRange) * C_THIRDPERSON_TRESHOLD_ZOOM) ){
+			zOffset = 1.5;
+		}
+		
 		// Angle
 		this.angle[0]    += ( this.angleFinal[0] - this.angle[0] ) * lerp * 2.0;
 		this.angle[1]    += ( this.angleFinal[1] - this.angle[1] ) * lerp * 2.0;
 		this.angle[0]    %=   360;
 		this.angle[1]    %=   360;
-
+		
 		// Find Camera direction (for NPC direction)
 		this.direction    = Math.floor( ( this.angle[1] + 22.5 ) / 45 ) % 8;
 
 		// Calculate new modelView mat
 		var matrix = this.modelView;
 		mat4.identity( matrix );
-		mat4.translateZ( matrix, (this.altitudeFrom - this.zoom) / 2 );
+		mat4.translateZ( matrix, (this.altitudeFrom - this.zoom) / 2);
 		mat4.rotateX( matrix, matrix, this.angle[0] / 180 * Math.PI );
 		mat4.rotateY( matrix, matrix, this.angle[1] / 180 * Math.PI );
 
 		// Center of the cell and inversed Y-Z axis
 		_position[0] = this.position[0] - 0.5;
-		_position[1] = this.position[2];
+		_position[1] = this.position[2] + zOffset;
 		_position[2] = this.position[1] - 0.5;
 		mat4.translate( matrix, matrix, _position );
 
