d4350ff0ac2b8bcd4446dd45c814bc4eed72d9c4
Antares <MrAntares@users.noreply.github.com>
2022-09-02 16:41:11 +0200

    Improved movement

--- src/Plugins/KeyToMove/KeyToMove.js
+++ src/Plugins/KeyToMove/KeyToMove.js
@@ -32,9 +32,15 @@ define(function( require )
 		DOWN: 	KEYS.DOWN
 	};
 	
+	//Multiple keys held
+	var keysDown = {};
+	
+	//Memory
+	var targetPos = [0, 0];
+	var keysDownTimeout;
 
 	//---- Now the job ----
-	function processKeyEvent( event ) {
+	function processKeyDownEvent( event ) {
 		if (event.which === MOVE.RIGHT || event.which === MOVE.LEFT || event.which === MOVE.UP || event.which === MOVE.DOWN) {
 			
 			// Skip if typing
@@ -49,28 +55,9 @@ define(function( require )
 			
 			if(Session.Entity){
 				event.stopImmediatePropagation();
-				// Get direction from keyboard
-				direction[0] = (event.which === MOVE.RIGHT ? +1 :
-								event.which === MOVE.LEFT  ? -1 :
-								0);
-
-				direction[1] = (event.which === MOVE.UP    ? +1 :
-								event.which === MOVE.DOWN  ? -1 :
-								0);
-
-				// Initialize matrix, based on Camera direction
-				mat2.identity(rotate);
-				mat2.rotate(rotate, rotate, -Camera.direction * 45 / 180 * Math.PI);
-
-				// Apply matrix to vector
-				vec2.transformMat2( direction, direction, rotate);
-
-				//Create move packed and add direction to current position then send packet
-				var pkt     = new PACKET.CZ.REQUEST_MOVE();
-				pkt.dest[0] = Math.round(Session.Entity.position[0] + direction[0]);
-				pkt.dest[1] = Math.round(Session.Entity.position[1] + direction[1]);
-				Network.sendPacket(pkt);
 				
+				keysDown[event.which] = true;
+				processKeysDown();
 				return false;
 			}
 			
@@ -81,10 +68,61 @@ define(function( require )
 		// Skip
 		return true;
 	}
+	
+	function processKeyUpEvent( event ) {
+		if (event.which === MOVE.RIGHT || event.which === MOVE.LEFT || event.which === MOVE.UP || event.which === MOVE.DOWN) {
+			delete keysDown[event.which];
+		}
+	}
+	
+	function processKeysDown(){
+		clearTimeout(keysDownTimeout);
+		
+		if(Session.Entity && Object.keys(keysDown).length > 0){
+			
+			direction[0] = 0;
+			direction[1] = 0;
+			
+			// Get direction from keyboard
+			direction[0] += (keysDown[MOVE.RIGHT] ? +1 : 0);
+			direction[0] += (keysDown[MOVE.LEFT]  ? -1 : 0);
+
+			direction[1] += (keysDown[MOVE.UP]    ? +1 : 0);
+			direction[1] += (keysDown[MOVE.DOWN]  ? -1 : 0);
+
+			// Initialize matrix, based on Camera direction
+			mat2.identity(rotate);
+			mat2.rotate(rotate, rotate, -Camera.direction * 45 / 180 * Math.PI);
+
+			// Apply matrix to vector
+			vec2.transformMat2( direction, direction, rotate);
+			
+			var newPos = [
+					Math.round(Session.Entity.position[0] + direction[0]),
+					Math.round(Session.Entity.position[1] + direction[1])
+				];
+			
+			//Create move packed and add direction to current position then send packet but only when not already moving there
+			if( targetPos[0] !== newPos[0] || targetPos[1] !== newPos[1] ){
+				targetPos[0] = newPos[0];
+				targetPos[1] = newPos[1];
+				var pkt     = new PACKET.CZ.REQUEST_MOVE();
+				pkt.dest[0] = newPos[0];
+				pkt.dest[1] = newPos[1];
+				Network.sendPacket(pkt);
+			}
+			
+			keysDownTimeout = setTimeout(processKeysDown, 10);
+		}
+	}
 
 	return function Init(){
 		jQuery(window).on('keydown.map', function( event ){
-				processKeyEvent(event);
+				processKeyDownEvent(event);
+		});
+		
+		jQuery(window).on('keyup.map', function( event ){
+				processKeyUpEvent(event);
 		});
 		
 		//Return true to signal successful initialization
