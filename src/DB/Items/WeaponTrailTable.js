a15de16773e02992964204783e57b89fe514d922
MrUnzO <kwon.unzo@gmail.com>
2022-12-15 15:50:28 +0700

    Weapon trail implemented
    (cherry picked from commit cc086db4edd3e9e001b4effaacc256fac5c752f9)

---
+++ src/DB/Items/WeaponTrailTable.js
@@ -0,0 +1,48 @@
+/**
+ * DB/Items/WeaponTrailTable.js
+ *
+ * Weapon trail table name
+ *
+ * This file is part of ROBrowser, Ragnarok Online in the Web Browser (http://www.robrowser.com/).
+ *
+ * @created MrUnzO
+ */
+
+define(["./WeaponType"], function (WeaponType) {
+	"use strict";
+
+	var WeaponTrail = {};
+
+	WeaponTrail[WeaponType.NONE] = "";
+	WeaponTrail[WeaponType.SHORTSWORD] = "_\xb4\xdc\xb0\xcb_\xb0\xcb\xb1\xa4"; //_´Ü°Ë_°Ë±¤ - dagger_trail
+	WeaponTrail[WeaponType.SWORD] = "_\xb0\xcb_\xb0\xcb\xb1\xa4"; //_°Ë_°Ë±¤ - sword_trail
+	WeaponTrail[WeaponType.TWOHANDSWORD] = "_\xb0\xcb_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.SPEAR] = "_\xc3\xa2_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.TWOHANDSPEAR] = "_\xc3\xa2_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.AXE] = "_\xb5\xb5\xb3\xa2_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.TWOHANDAXE] = "_\xb5\xb5\xb3\xa2_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.MACE] = "_\xc5\xac\xb7\xb4_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.TWOHANDMACE] = "_\xc5\xac\xb7\xb4_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.ROD] = "_\xb7\xd4\xb5\xe5_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.BOW] = "_\xc8\xb0_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.KNUKLE] = "_\xb3\xca\xc5\xac_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.INSTRUMENT] = "_\xbe\xc7\xb1\xe2_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.WHIP] = "_\xc3\xa4\xc2\xef_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.BOOK] = "_\xc3\xa5_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.KATAR] = "_\xc4\xab\xc5\xb8\xb8\xa3\x5f\xc4\xab\xc5\xb8\xb8\xa3_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.GUN_HANDGUN] = "_\xb1\xc7\xc3\xd1_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.GUN_RIFLE] = "_\xb1\xe2\xb0\xfc\xc3\xd1_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.GUN_GATLING] = "_\xb1\xe2\xb0\xfc\xc3\xd1_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.GUN_SHOTGUN] = "_\xb1\xe2\xb0\xfc\xc3\xd1_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.GUN_GRANADE] = "_\xb1\xe2\xb0\xfc\xc3\xd1_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.SYURIKEN] = "_\xbc\xf6\xb8\xae\xb0\xcb_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.TWOHANDROD] = "_\xb7\xd4\xb5\xe5_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.SHORTSWORD_SHORTSWORD] = "_\xb4\xdc\xb0\xcb\x5f\xb4\xdc\xb0\xcb_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.SWORD_SWORD] = "_\xb0\xcb\x5f\xb0\xcb_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.AXE_AXE] = "_\xb5\xb5\xb3\xa2\x5f\xb5\xb5\xb3\xa2_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.SHORTSWORD_SWORD] = "_\xb4\xdc\xb0\xcb\x5f\xb0\xcb_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.SHORTSWORD_AXE] = "_\xb4\xdc\xb0\xcb\x5f\xb5\xb5\xb3\xa2_\xb0\xcb\xb1\xa4";
+	WeaponTrail[WeaponType.SWORD_AXE] = "_\xb0\xcb\x5f\xb5\xb5\xb3\xa2_\xb0\xcb\xb1\xa4";
+
+	return WeaponTrail;
+});
