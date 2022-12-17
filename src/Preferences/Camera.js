6011bfd13ec7b6e7e41438d0d948e652c4ba37d1
MrUnzO <kwon.unzo@gmail.com>
2022-12-18 04:09:39 +0700

    Camera fixing
    (cherry picked from commit d0a4058691895dcde24af8811d7d213564381046)

--- src/Preferences/Camera.js
+++ src/Preferences/Camera.js
@@ -17,6 +17,7 @@ define( ['Core/Preferences'], function( Preferences )
 	 */
 	return Preferences.get( 'Camera', {
 		smooth:  true,
-		zoom:    125.0
+		zoom:    125.0,
+		indoorZoom:    125.0
 	}, 1.1 );
 });
