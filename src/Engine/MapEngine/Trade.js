63f0cd24acb23732660640ea1fd9c1018a63ba6d
Antares <MrAntares@users.noreply.github.com>
2023-09-18 16:20:25 +0200

    Update Trade.js

--- src/Engine/MapEngine/Trade.js
+++ src/Engine/MapEngine/Trade.js
@@ -80,16 +80,16 @@ define(function( require )
 	{
 		switch (pkt.result) {
 			case 0: // Char is too far
-				ChatBox.addText( DB.getMessage(70), ChatBox.TYPE.ERROR);
+				ChatBox.addText( DB.getMessage(70), ChatBox.TYPE.ERROR, ChatBox.FILTER.PUBLIC_LOG);
 				break;
 
 
 			case 1: // Character does not exist
-				ChatBox.addText( DB.getMessage(71), ChatBox.TYPE.ERROR);
+				ChatBox.addText( DB.getMessage(71), ChatBox.TYPE.ERROR, ChatBox.FILTER.PUBLIC_LOG);
 				break;
 
 			case 2: // In another deal
-				ChatBox.addText( DB.getMessage(72), ChatBox.TYPE.ERROR);
+				ChatBox.addText( DB.getMessage(72), ChatBox.TYPE.ERROR, ChatBox.FILTER.PUBLIC_LOG);
 				break;
 
 			case 3:
@@ -100,7 +100,7 @@ define(function( require )
 				break;
 
 			case 4: // Cancel
-				ChatBox.addText( DB.getMessage(74), ChatBox.TYPE.ERROR);
+				ChatBox.addText( DB.getMessage(74), ChatBox.TYPE.ERROR, ChatBox.FILTER.PUBLIC_LOG);
 				break;
 
 			case 5: // AFK ?
@@ -134,11 +134,11 @@ define(function( require )
 	{
 		switch (pkt.result) {
 			case 1: // overweight
-				ChatBox.addText( DB.getMessage(73), ChatBox.TYPE.ERROR);
+				ChatBox.addText( DB.getMessage(73), ChatBox.TYPE.ERROR, ChatBox.FILTER.PUBLIC_LOG);
 				break;
 
 			case 2: // trade canceled
-				ChatBox.addText( DB.getMessage(74), ChatBox.TYPE.ERROR);
+				ChatBox.addText( DB.getMessage(74), ChatBox.TYPE.ERROR, ChatBox.FILTER.PUBLIC_LOG);
 				break;
 		}
 
@@ -174,7 +174,7 @@ define(function( require )
 	 */
 	function onTradeCancel( pkt )
 	{
-		ChatBox.addText( DB.getMessage(74), ChatBox.TYPE.ERROR);
+		ChatBox.addText( DB.getMessage(74), ChatBox.TYPE.ERROR, ChatBox.FILTER.PUBLIC_LOG);
 		Trade.remove();
 	}
 
@@ -219,13 +219,13 @@ define(function( require )
 	{
 		// Fail
 		if (pkt.result === 1) {
-			ChatBox.addText( DB.getMessage(76), ChatBox.TYPE.ERROR);
+			ChatBox.addText( DB.getMessage(76), ChatBox.TYPE.ERROR, ChatBox.FILTER.PUBLIC_LOG);
 			Trade.remove();
 			return;
 		}
 
 		//TODO: Give items...
-		ChatBox.addText( DB.getMessage(75), ChatBox.TYPE.BLUE);
+		ChatBox.addText( DB.getMessage(75), ChatBox.TYPE.BLUE, ChatBox.FILTER.PUBLIC_LOG);
 		Trade.remove();
 	}
 
