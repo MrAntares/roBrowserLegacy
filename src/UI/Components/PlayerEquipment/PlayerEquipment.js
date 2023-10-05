0b4b545ced802d4386afadc3dfc6103ad9e36123
Antares <MrAntares@users.noreply.github.com>
2023-10-05 13:31:45 +0200

    Create PlayerEquipment.js

---
+++ src/UI/Components/PlayerEquipment/PlayerEquipment.js
@@ -0,0 +1,36 @@
+/**
+ * UI/Components/PlayerEquipment/PlayerEquipment.js
+ *
+ * Other player's Equipment window
+ *
+ * This file is part of ROBrowser, (http://www.robrowser.com/).
+ *
+ */
+define(function(require)
+{
+	'use strict';
+
+
+	/**
+	 * Dependencies
+	 */
+	var DB                 = require('DB/DBManager');
+	var Renderer           = require('Renderer/Renderer');
+	var Camera             = require('Renderer/Camera');
+	var SpriteRenderer     = require('Renderer/SpriteRenderer');
+	var UIManager          = require('UI/UIManager');
+	var UIComponent        = require('UI/UIComponent');
+	var Equipment          = require('UI/Components/Equipment/Equipment');
+	var jQuery             = require('Utils/jquery');
+
+
+	/**
+	 * Create Component
+	 */
+	var PlayerEquipment = Equipment.clone('PlayerEquipment');
+
+	/**
+	 * Create component and export it
+	 */
+	return UIManager.addComponent(PlayerEquipment);
+});
