baeb56137233a15002ecd46f45ecd3d5acde21cf
Alison Serafim <alisonserafim@gmail.com>
2023-11-23 22:39:18 -0300

    feat: implement search

--- src/UI/Components/Rodex/Rodex.js
+++ src/UI/Components/Rodex/Rodex.js
@@ -15,12 +15,13 @@ define(function (require) {
 	 */
 	var DB = require('DB/DBManager');
 	var jQuery = require('Utils/jquery');
+	var Client = require('Core/Client');
 	var Preferences = require('Core/Preferences');
 	var Renderer = require('Renderer/Renderer');
 	var ChatBox = require('UI/Components/ChatBox/ChatBox');
 	var UIManager = require('UI/UIManager');
 	var UIComponent = require('UI/UIComponent');
-	
+
 	var htmlText = require('text!./Rodex.html');
 	var cssText = require('text!./Rodex.css');
 
@@ -96,12 +97,14 @@ define(function (require) {
 		this.ui.find('.previous-page').on('click', onClickPreviousPage);
 		this.ui.find('.next-page').on('click', onClickNexPage);
 		this.ui.find('.nav-item').on('click', onClickTab);
+		this.ui.find('.search-title').on('click', onClickSearchTitle);
+		this.ui.find('.search-sender').on('click', onClickSearchSender);
+		this.ui.find('.search-btn').on('click', onClickSearchButton);
 
 		Rodex.openType = 0;
 		Rodex.ui.find('.nav-item.active').removeClass('active');
 		Rodex.ui.find('#tab_0').addClass('active');
 		Rodex.searchType = 1;
-		
 	};
 
 	/**
@@ -133,10 +136,21 @@ define(function (require) {
 		Rodex.ui.focus();
 	};
 
-	Rodex.createRodexList = function createRodexList(tabID = 0) {
+	Rodex.createRodexList = function createRodexList(tabID = 0, search = false, term = "") {
 		var content = Rodex.ui.find('.mail-list');
 		content.html('');
-		let mail_list = Rodex.getMailsByTabID(tabID);
+		let mail_list = [];
+		if (search) {
+			Rodex.page = 0;
+			if (Rodex.searchType == 1) {
+				mail_list = Rodex.getMailsByTitle(term);
+			} else {
+				mail_list = Rodex.getMailsBySender(term);
+
+			}
+		} else {
+			mail_list = Rodex.getMailsByTabID(tabID);
+		}
 		if (mail_list.length == 0) return;
 		let start = Rodex.pageSize * Rodex.page;
 
@@ -180,26 +194,34 @@ define(function (require) {
 		return Rodex.list.filter(mail => mail.openType == tabID);
 	}
 
+	Rodex.getMailsBySender = function getMailsBySender(term) {
+		return Rodex.list.filter(mail => mail.SenderName.indexOf(term) > -1);
+	}
+
+	Rodex.getMailsByTitle = function getMailsByTitle(term) {
+		return Rodex.list.filter(mail => mail.title.indexOf(term) > -1);
+	}
+
 	Rodex.getMailByID = function getMailByID(mailID) {
 		return Rodex.list.find(mail => mail.MailID == mailID);
 	}
 
 	Rodex.getAll = function getAll() {
-		for(let i = 0; i < Rodex.list.length; i++) {
+		for (let i = 0; i < Rodex.list.length; i++) {
 			let mail = Rodex.list[i];
-			if(mail.type > 0 && (mail.type == 4 || mail.type == 6)) {
+			if (mail.type > 0 && (mail.type == 4 || mail.type == 6)) {
 				Rodex.requestItemsFromRodex(mail.openType, mail.MailID);
 			}
-			if(mail.type > 0 && (mail.type == 2 || mail.type == 6)) {
+			if (mail.type > 0 && (mail.type == 2 || mail.type == 6)) {
 				Rodex.requestZenyFromRodex(mail.openType, mail.MailID);
 			}
 		}
 	}
 
 	Rodex.deleteAll = function deleteAll() {
-		for(let i = 0; i < Rodex.list.length; i++) {
+		for (let i = 0; i < Rodex.list.length; i++) {
 			let mail = Rodex.list[i];
-			if(mail.type == 0) { // delete only empty mails
+			if (mail.type == 0) { // delete only empty mails
 				Rodex.requestDeleteRodex(mail.openType, mail.MailID);
 			} else {
 				ChatBox.addText(DB.getMessage(2612), ChatBox.TYPE.INFO_MAIL, ChatBox.FILTER.PUBLIC_LOG);
@@ -263,14 +285,14 @@ define(function (require) {
 
 	function onClickPreviousPage(e) {
 		e.stopImmediatePropagation();
-		if(Rodex.page - 1 < 0 ) return;
+		if (Rodex.page - 1 < 0) return;
 		Rodex.page--;
 		Rodex.createRodexList(Rodex.openType);
 	}
 
 	function onClickNexPage(e) {
 		e.stopImmediatePropagation();
-		if((Rodex.page + 1) * Rodex.pageSize < Rodex.getMailsByTabID(Rodex.openType).length) {
+		if ((Rodex.page + 1) * Rodex.pageSize < Rodex.getMailsByTabID(Rodex.openType).length) {
 			Rodex.page++;
 			Rodex.createRodexList(Rodex.openType);
 		}
@@ -278,16 +300,50 @@ define(function (require) {
 
 	function onClickTab(e) {
 		e.stopImmediatePropagation();
+		Rodex.page = 0;
 		let element = jQuery(e.currentTarget);
 		let tid = element.attr('id');
 		let id = tid.replace("tab_", "");
 		Rodex.ui.find('.nav-item.active').removeClass('active');
 		element.addClass('active');
-		if(id >= 0 && id <= 2) {
+		if (id >= 0 && id <= 2) {
 			Rodex.openType = id;
 			Rodex.createRodexList(Rodex.openType);
 		} else {
 			Rodex.ui.find('.mail-list').html(''); // search tab
+		}
+	}
+
+	function onClickSearchTitle(e) {
+		e.stopImmediatePropagation();
+		Rodex.searchType = 1;
+		Client.loadFile(DB.INTERFACE_PATH + 'basic_interface/rodexsystem/renewal/checkbox_search_off.bmp', function (data) {
+			Rodex.ui.find('.search-sender').css('backgroundImage', 'url(' + data + ')');
+		});
+		Client.loadFile(DB.INTERFACE_PATH + 'basic_interface/rodexsystem/renewal/checkbox_search_on.bmp', function (data) {
+			Rodex.ui.find('.search-title').css('backgroundImage', 'url(' + data + ')');
+		});
+	}
+
+	function onClickSearchSender(e) {
+		e.stopImmediatePropagation();
+		Rodex.searchType = 2;
+
+		Client.loadFile(DB.INTERFACE_PATH + 'basic_interface/rodexsystem/renewal/checkbox_search_on.bmp', function (data) {
+			Rodex.ui.find('.search-sender').css('backgroundImage', 'url(' + data + ')');
+		});
+		Client.loadFile(DB.INTERFACE_PATH + 'basic_interface/rodexsystem/renewal/checkbox_search_off.bmp', function (data) {
+			Rodex.ui.find('.search-title').css('backgroundImage', 'url(' + data + ')');
+		});
+	}
+
+	function onClickSearchButton(e) {
+		e.stopImmediatePropagation();
+		let search = Rodex.ui.find('.search').val();
+		Rodex.ui.find('.nav-item.active').removeClass('active');
+		Rodex.ui.find('#tab_3').addClass('active');
+		if (search.length > 2) {
+			Rodex.createRodexList(0, true, search);
 		}
 	}
 
