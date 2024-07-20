b13dba861ee0049751fe894f82747ac6739715c5
Samuel23 <johnsamuel_santos3@yahoo.com>
2024-07-20 11:50:47 +0800

    Standardized Item Name with Item Options overlay in Cart

--- src/UI/Components/CartItems/CartItems.js
+++ src/UI/Components/CartItems/CartItems.js
@@ -612,6 +612,13 @@ define(function(require)
 			return;
 		}
 
+		let quantity = ' ea';
+		if ((item.type === ItemType.WEAPON || item.type === ItemType.EQUIP) && 
+			item.Options.filter(Option => Option.index !== 0).length > 0)
+		{
+			quantity = ' Quantity';
+		}
+
 		// Get back data
 		var pos     = jQuery(this).position();
 		var overlay = CartItems.ui.find('.overlay');
@@ -619,7 +626,7 @@ define(function(require)
 		// Display box
 		overlay.show();
 		overlay.css({top: pos.top, left:pos.left+35});
-		overlay.text(DB.getItemName(item) + ' ' + (item.count || 1) + ' ea');
+		overlay.text(DB.getItemName(item) + ': ' + (item.count || 1) + quantity);
 
 		if (item.IsIdentified) {
 			overlay.removeClass('grey');
