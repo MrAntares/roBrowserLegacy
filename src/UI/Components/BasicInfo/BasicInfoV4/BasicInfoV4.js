f7813b8149274c675afe46c7514b7459c627a11e
Andre Cavallaro <andre.mannheim@googlemail.com>
2024-07-21 21:26:29 +0200

    Fixed cursor, background and height for basic info buttons

--- src/UI/Components/BasicInfo/BasicInfoV4/BasicInfoV4.js
+++ src/UI/Components/BasicInfo/BasicInfoV4/BasicInfoV4.js
@@ -90,7 +90,7 @@ define(function(require)
 		this.ui.find('.topbar .right').click(BasicInfoV4.toggleMode.bind(this));
 		this.ui.find('.toggle_btns').click(BasicInfoV4.toggleButtons.bind(this));
 
-		this.ui.find('.buttons div').click(function(){
+		this.ui.find('.buttons button').click(function(){
 			switch (this.id) {
 				case 'item':
 					Inventory.getUI().toggle();
