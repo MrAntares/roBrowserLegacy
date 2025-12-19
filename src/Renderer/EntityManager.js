ec2a00beae3c8a2a0360fe7066e4eb5a4535a182
Roger <darklord.dl@gmail.com>
2025-12-19 17:43:19 -0300

    added EntityManager.js Spatial Grid

--- src/Renderer/EntityManager.js
+++ src/Renderer/EntityManager.js
@@ -18,9 +18,110 @@ define(function (require) {
 	var Mouse          = require('Controls/MouseEventHandler');
 	var KEYS           = require('Controls/KeyEventHandler');
 	var PathFinding    = require('Utils/PathFinding');
+	var GraphicsSettings = require('Preferences/Graphics');
 	var Altitude       = require('Renderer/Map/Altitude');
+	var glMatrix       = require('Utils/gl-matrix');
 
+	var vec3 = glMatrix.vec3;
 	var _list = [];
+
+
+	/**
+	 * Spatial Grid for optimization
+	 */
+	var SpatialGrid = {
+		cellSize: GraphicsSettings.culling ? GraphicsSettings.viewArea : 20,
+		cells: {},
+		
+		/**
+		 * Clear the grid
+		 */
+		clear: function() {
+			this.cells = {};
+		},
+
+		/**
+		 * Add entity to the grid
+		 */
+		add: function(entity) {
+			// Simple spatial hashing
+			var cx = Math.floor(entity.position[0] / this.cellSize);
+			var cy = Math.floor(entity.position[1] / this.cellSize);
+			var key = cx + "_" + cy;
+
+			if (!this.cells[key]) {
+				this.cells[key] = [];
+			}
+			this.cells[key].push(entity);
+		},
+
+		/**
+		 * Get entities visible to the camera
+		 * @param {object} gl - WebGL Context
+		 * @return {Array} list of visible entities
+		 */
+		getVisibleEntities: function(gl) {
+			var visibleEntities = [];
+			
+			// Get Camera info (Approximation for culling)
+			// We assume the camera is looking at a target (Player)
+			// A simple distance check or frustum check is needed.
+			// Since we don't have full frustum planes readily available without calculation,
+			// we use a generous radius around the camera target/center.
+			
+			var center = [0, 0, 0];
+			var rangeX = GraphicsSettings.culling ? this.cellSize : 20;  // Horizontal visibility range (cells)
+			var rangeY = GraphicsSettings.culling ? this.cellSize : 20;  // Vertical visibility range (cells)
+
+			// Heuristic: Use Session Entity or Camera Target as center
+			if (Session.Entity && Session.Entity.position) {
+				center = Session.Entity.position;
+			} else {
+				// Fallback if no entity
+				return _list; 
+			}
+
+			var startCx = Math.floor((center[0] - rangeX) / this.cellSize);
+			var endCx   = Math.floor((center[0] + rangeX) / this.cellSize);
+			var startCy = Math.floor((center[1] - rangeY) / this.cellSize);
+			var endCy   = Math.floor((center[1] + rangeY) / this.cellSize);
+
+			for (var y = startCy; y <= endCy; ++y) {
+				for (var x = startCx; x <= endCx; ++x) {
+					var key = x + "_" + y;
+					if (this.cells[key]) {
+						var cellEntities = this.cells[key];
+						for(var i = 0; i < cellEntities.length; i++) {
+							visibleEntities.push(cellEntities[i]);
+						}
+					}
+				}
+			}
+
+			return visibleEntities;
+		},
+
+		/**
+		 * Get entities near a specific point (for intersection)
+		 */
+		getNearbyEntities: function(x, y, range) {
+			var nearEntities = [];
+			var cellRange = Math.ceil(range / this.cellSize);
+			
+			var cx = Math.floor(x / this.cellSize);
+			var cy = Math.floor(y / this.cellSize);
+
+			for (var i = cy - cellRange; i <= cy + cellRange; i++) {
+				for (var j = cx - cellRange; j <= cx + cellRange; j++) {
+					var key = j + "_" + i;
+					if (this.cells[key]) {
+						nearEntities.push.apply(nearEntities, this.cells[key]);
+					}
+				}
+			}
+			return nearEntities;
+		}
+	};
 
 	/**
 	 * Find an Entity and return its index
@@ -115,6 +216,7 @@ define(function (require) {
 		}
 
 		_list.length = 0;
+		SpatialGrid.clear();
 	}
 
 
@@ -260,33 +362,49 @@ define(function (require) {
 		if (!_list.length) {
 			return;
 		}
+		SpatialGrid.cellSize = GraphicsSettings.culling ? GraphicsSettings.viewArea : 20;
+		// Note: Rebuilding grid every frame is O(N), but usually N is < 1000.
+		// This allows us to sort only visible entities O(K log K) where K << N.
+		SpatialGrid.clear();
+		for(i = 0, count = _list.length; i < count; ++i) {
+			SpatialGrid.add(_list[i]);
+		}
 
-		_list.sort(sort);
+		// Get only visible entities
+		var visibleList = SpatialGrid.getVisibleEntities(gl);
+		
+		visibleList.sort(sort);
 
 		// Use program
 		SpriteRenderer.bind3DContext(gl, modelView, projection, fog);
 
 		// Rendering
-		for (i = 0, count = _list.length; i < count; ++i) {
-			if ((_list[i].objecttype != _list[i].constructor.TYPE_EFFECT && !renderEffects) || (_list[i].objecttype == _list[i].constructor.TYPE_EFFECT && renderEffects)) {
+		for (i = 0, count = visibleList.length; i < count; ++i) {
+			var entity = visibleList[i];
+
+			if ((entity.objecttype != entity.constructor.TYPE_EFFECT && !renderEffects) || (entity.objecttype == entity.constructor.TYPE_EFFECT && renderEffects)) {
 				// Remove from list
-				if (_list[i].remove_tick && _list[i].remove_tick + _list[i].remove_delay < tick) {
+				if (entity.remove_tick && entity.remove_tick + entity.remove_delay < tick) {
 
 					// Remove focus
 					var entityFocus = getFocusEntity();
-					if (entityFocus && entityFocus.GID === _list[i].GID) {
+					if (entityFocus && entityFocus.GID === entity.GID) {
 						entityFocus.onFocusEnd();
 						setFocusEntity(null);
 					}
 
-					_list[i].clean();
-					_list.splice(i, 1);
-					i--;
-					count--;
+					entity.clean();
+					
+					// We need to find and remove from the main list, not the visible list
+					var idx = _list.indexOf(entity);
+					if (idx > -1) {
+						_list.splice(idx, 1);
+					}
+					
 					continue;
 				}
 
-				_list[i].render(modelView, projection);
+				entity.render(modelView, projection);
 			}
 		}
 
@@ -305,14 +423,26 @@ define(function (require) {
 		if (!_list.length) {
 			return;
 		}
+		var candidates = [];
+		var center = Session.Entity.position; // [x, y, z]
+		var range = GraphicsSettings.culling ? GraphicsSettings.viewArea : 20;
+		
+		if (!center) return null;
 
-		_list.sort(sortByPriority);
+		for (i = 0, count = _list.length; i < count; ++i) {
+			entity = _list[i];
+			if (Math.abs(entity.position[0] - center[0]) < range && Math.abs(entity.position[1] - center[1]) < range) {
+				candidates.push(entity);
+			}
+		}
+
+		candidates.sort(sortByPriority);
 
 		var x = Mouse.screen.x;
 		var y = Mouse.screen.y;
 
-		for (i = 0, count = _list.length; i < count; ++i) {
-			entity = _list[i];
+		for (i = 0, count = candidates.length; i < count; ++i) {
+			entity = candidates[i];
 
 			// No picking on dead entites
 			if ((entity.action !== entity.ACTION.DIE || entity.objecttype === Entity.TYPE_PC) && entity.remove_tick === 0) {
@@ -338,14 +468,23 @@ define(function (require) {
 		var closestEntity = false;
 		var distance      = Infinity;
 
+		// Optimization: Use simple distance check first before expensive pathfinding
 		_list.forEach((entity) => {
 			if (entity.GID !== sourceEntity.GID && entity.objecttype === type && entity.action !== entity.ACTION.DIE && entity.remove_tick === 0) {
+				
+				// Quick distance check (Manhattan or Euclidean)
+				var distSq = Math.pow(entity.position[0] - sourceEntity.position[0], 2) + Math.pow(entity.position[1] - sourceEntity.position[1], 2);
+				if (distSq > 400) return; // Ignore entities > 20 cells away
+
 				var dst = Infinity;
 				if (closestEntity) {
-					dst = getPathDistance(sourceEntity, entity);
-					if (dst && dst < distance) {
-						closestEntity = entity;
-						distance      = dst;
+					// Only pathfind if potentially closer than current best
+					if (distSq < distance * distance) {
+						dst = getPathDistance(sourceEntity, entity);
+						if (dst && dst < distance) {
+							closestEntity = entity;
+							distance      = dst;
+						}
 					}
 				} else {
 					dst = getPathDistance(sourceEntity, entity);
