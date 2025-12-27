36ccd3d27c42b9fe9ef0245ddfdd873a3e3e3155
AoShinHo <126742159+AoShinRO@users.noreply.github.com>
2025-12-27 20:48:57 -0300

    Fix EntityAttachments not rendering after depth correction patch (#681)
    * fix entity attachments not rendering due depth mask
    add blending to torch
    
    * improved torchvar setup

--- src/Renderer/Entity/EntityAttachments.js
+++ src/Renderer/Entity/EntityAttachments.js
@@ -225,10 +225,12 @@ function(     Client,            Renderer,            SpriteRenderer,           
 			}
 
 			// render layers
+			SpriteRenderer.setDepthMask(false);
+			var useBlendOne = (attachment.file === 'torch_01');
 			for (i = 0, count = layers.length; i < count; ++i) {
-				this.entity.renderLayer(layers[i], spr, spr, 1.0, position, false);
+				this.entity.renderLayer(layers[i], spr, spr, 1.0, position, false, useBlendOne);
 			}
-
+			SpriteRenderer.setDepthMask(true);
 			return clean;
 		};
 	}();
