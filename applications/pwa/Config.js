349a38440430b1ecef1af79c9bd3f401d3a64b47
pfak <pfak@users.noreply.github.com>
2026-01-25 13:05:05 -0800

    feat: externalize ROConfig into separate Config.js file
    Move configuration out of inline HTML into Config.js with support for
    Config.local.js overrides. This allows default configs to be updated
    during deployments while preserving local server-specific settings.
    
    - Add Config.js with window.ROConfigBase for default settings
    - Add Config.local.js.example as a template for local overrides
    - Update builder-web.js to generate Config.js during build
    - Update index.html to load and deep-merge both config files
    - Update Getting Started guide with configuration documentation
    - Add Config.local.js to .gitignore

---
+++ applications/pwa/Config.js
@@ -0,0 +1,63 @@
+/**
+ * ROBrowser Configuration - Default Settings
+ *
+ * This file contains default configuration values.
+ * To override settings without modifying this file, create Config.local.js
+ * with your custom values in window.ROConfigLocal.
+ *
+ * Example Config.local.js:
+ *   window.ROConfigLocal = {
+ *       servers: [{ display: 'My Server', address: '192.168.1.1', ... }],
+ *       skipIntro: true
+ *   };
+ */
+window.ROConfigBase = {
+	type: 'FRAME',
+	application: 'ONLINE',
+	development: true,
+	remoteClient: 'https://grf.robrowser.com/',
+	servers: [
+		{
+			display: 'roBrowser Demo Server',
+			desc: 'demo server',
+			address: '127.0.0.1',
+			port: 6900,
+			version: 25,
+			langtype: 12,
+			packetver: 20130618,
+			renewal: false,
+			worldMapSettings: { episode: 12 },
+			packetKeys: false,
+			socketProxy: 'wss://connect.robrowser.com',
+			adminList: [2000000]
+		},
+		// ADD PUBLIC TEST SERVERS HERE WITH _M _F REGISTRATION
+	],
+	packetDump: false,
+	skipServerList: true,
+	skipIntro: false,
+	aura: {},
+	autoLogin: [],
+	BGMFileExtension: ['mp3'],
+	calculateHash: false,
+	CameraMaxZoomOut: 5,
+	charBlockSize: 0,
+	clientHash: null,
+	clientVersionMode: "PacketVer",
+	disableConsole: false,
+	enableBank: false,
+	enableCashShop: false,
+	enableCheckAttendance: false,
+	enableDmgSuffix: false,
+	enableHomunAutoFeed: false,
+	enableMapName: false,
+	FirstPersonCamera: false,
+	grfList: null,
+	hashFiles: [],
+	loadLua: false,
+	onReady: null,
+	plugins: {},
+	registrationweb: '',
+	saveFiles: true,
+	ThirdPersonCamera: false,
+};
