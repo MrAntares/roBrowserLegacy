58aa123957fd5808fd587c7033fe016a5a906684
Antares <MrAntares@users.noreply.github.com>
2026-02-12 16:36:11 +0100

    Fix packetver lint warnings and add defaulting.

--- src/Network/PacketLength.js
+++ src/Network/PacketLength.js
@@ -20,53 +20,56 @@ define(function (require) {
 	 * @param {number} packetver - packet version (clientdate)
 	 */
 	function init(packetver) {
+		var Lengths;
 		packetver = parseInt(packetver);
 		if (packetver >= 20250000) {
-			var Lengths = require('./Packets/packets2025_len_main');
+			Lengths = require('./Packets/packets2025_len_main');
 		} else if (packetver >= 20240000) {
-			var Lengths = require('./Packets/packets2024_len_main');
+			Lengths = require('./Packets/packets2024_len_main');
 		} else if (packetver >= 20230000) {
-			var Lengths = require('./Packets/packets2023_len_main');
+			Lengths = require('./Packets/packets2023_len_main');
 		} else if (packetver >= 20220000) {
-			var Lengths = require('./Packets/packets2022_len_main');
+			Lengths = require('./Packets/packets2022_len_main');
 		} else if (packetver >= 20210000) {
-			var Lengths = require('./Packets/packets2021_len_main');
+			Lengths = require('./Packets/packets2021_len_main');
 		} else if (packetver >= 20200000) {
-			var Lengths = require('./Packets/packets2020_len_main');
+			Lengths = require('./Packets/packets2020_len_main');
 		} else if (packetver >= 20190000) {
-			var Lengths = require('./Packets/packets2019_len_main');
+			Lengths = require('./Packets/packets2019_len_main');
 		} else if (packetver >= 20180000) {
-			var Lengths = require('./Packets/packets2018_len_main');
+			Lengths = require('./Packets/packets2018_len_main');
 		} else if (packetver >= 20170000) {
-			var Lengths = require('./Packets/packets2017_len_main');
+			Lengths = require('./Packets/packets2017_len_main');
 		} else if (packetver >= 20160000) {
-			var Lengths = require('./Packets/packets2016_len_main');
+			Lengths = require('./Packets/packets2016_len_main');
 		} else if (packetver >= 20150000) {
-			var Lengths = require('./Packets/packets2015_len_main');
+			Lengths = require('./Packets/packets2015_len_main');
 		} else if (packetver >= 20140000) {
-			var Lengths = require('./Packets/packets2014_len_main');
+			Lengths = require('./Packets/packets2014_len_main');
 		} else if (packetver >= 20130000) {
-			var Lengths = require('./Packets/packets2013_len_main');
+			Lengths = require('./Packets/packets2013_len_main');
 		} else if (packetver >= 20120000) {
-			var Lengths = require('./Packets/packets2012_len_main');
+			Lengths = require('./Packets/packets2012_len_main');
 		} else if (packetver >= 20110000) {
-			var Lengths = require('./Packets/packets2011_len_main');
+			Lengths = require('./Packets/packets2011_len_main');
 		} else if (packetver >= 20100000) {
-			var Lengths = require('./Packets/packets2010_len_main');
+			Lengths = require('./Packets/packets2010_len_main');
 		} else if (packetver >= 20090000) {
-			var Lengths = require('./Packets/packets2009_len_main');
+			Lengths = require('./Packets/packets2009_len_main');
 		} else if (packetver >= 20080000) {
-			var Lengths = require('./Packets/packets2008_len_main');
+			Lengths = require('./Packets/packets2008_len_main');
 		} else if (packetver >= 20070000) {
-			var Lengths = require('./Packets/packets2007_len_main');
+			Lengths = require('./Packets/packets2007_len_main');
 		} else if (packetver >= 20060000) {
-			var Lengths = require('./Packets/packets2006_len_main');
+			Lengths = require('./Packets/packets2006_len_main');
 		} else if (packetver >= 20050000) {
-			var Lengths = require('./Packets/packets2005_len_main');
+			Lengths = require('./Packets/packets2005_len_main');
 		} else if (packetver >= 20040000) {
-			var Lengths = require('./Packets/packets2004_len_main');
+			Lengths = require('./Packets/packets2004_len_main');
 		} else if (packetver >= 20030000) {
-			var Lengths = require('./Packets/packets2003_len_main');
+			Lengths = require('./Packets/packets2003_len_main');
+		} else {
+			Lengths = require('./Packets/packets2003_len_main'); // Defaulting to 2003 if packetver is older or something weird.
 		}
 		packets_len = Lengths.init(packetver);
 		console.log('%c[Network] Packet Length initialized ', 'color:#007000', packetver);
