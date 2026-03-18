551a687ec90de7f06cf939c6fe541ff3ba4b2187
AoShinHo <darklord.dl@gmail.com>
2026-03-18 18:02:11 -0300

    change comma

--- src/DB/DBManager.js
+++ src/DB/DBManager.js
@@ -231,8 +231,8 @@ define(function (require) {
 	var servers = Configs.get('servers', []);
 	var langType = servers[0] && servers[0].langtype ? parseInt(servers[0].langtype, 10) : 1;
 
-	// use userStringDecoder instead
-	var _userCharpage = TextEncoding.detectEncodingByLangtype(langType, Configs.get('disableKorean'));
+	// setup default encoding
+	TextEncoding.detectEncodingByLangtype(langType, Configs.get('disableKorean'));
 
 	// create decoders
 	let userStringDecoder = TextEncoding;
