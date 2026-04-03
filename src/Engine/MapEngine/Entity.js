5b6d0f460236171429f6b2494bdcd6b46eca9238
AoShinHo <darklord.dl@gmail.com>
2026-04-02 21:27:50 -0300

    adds multi status block support

--- src/Engine/MapEngine/Entity.js
+++ src/Engine/MapEngine/Entity.js
@@ -2418,11 +2418,7 @@ function onEntityStatusChange(pkt) {
 			break;
 	}
 
-	const guardStatuses = [StatusConst.KYRIE, StatusConst.AUTOGUARD, StatusConst.PARRYING];
-
-	if (guardStatuses.includes(pkt.index)) {
-		entity.isGuard = pkt.state;
-	}
+	processBlockStatus(entity, pkt);
 
 	// Modify icon
 	if (entity === Session.Entity) {
@@ -2868,6 +2864,19 @@ function onHatEffects(pkt) {
 	}
 }
 
+function processBlockStatus(entity, pkt) {  
+	const guardStatuses = [StatusConst.KYRIE, StatusConst.AUTOGUARD, StatusConst.PARRYING];  
+  
+	if (guardStatuses.includes(pkt.index)) {  
+		if (!entity._guardStatuses) entity._guardStatuses = new Set();  
+		if (pkt.state && !entity._guardStatuses.has(pkt.index)) {  
+			entity._guardStatuses.add(pkt.index);  
+		} else if (!pkt.state) {  
+			entity._guardStatuses.delete(pkt.index);  
+		}  
+		entity.isGuard = entity._guardStatuses.size > 0;  
+	}  
+}
 /**
  * Initialize
  */
