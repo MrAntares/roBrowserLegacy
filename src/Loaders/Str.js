271fa3d6531894d74197e6dc557c97a22ad23e56
AoShinHo <darklord.dl@gmail.com>
2026-04-03 10:26:39 -0300

    update Str.js to class

--- src/Loaders/Str.js
+++ src/Loaders/Str.js
@@ -12,52 +12,50 @@
 import BinaryReader from 'Utils/BinaryReader.js';
 
 /**
- * Str class loader
+ * Frame structure
  *
- * @param {ArrayBuffer} data - optional
- * @param {string} texturePath - optional
+ * @param {BinaryReader} fp
  */
-function STR(data, texturePath) {
-	this.version = 0.0;
-	this.texturePath = texturePath ?? '';
-
-	if (data) {
-		this.load(data);
+class STRAnimation {
+	constructor(fp) {
+		this.frame = fp.readLong();
+		this.type = fp.readULong();
+		this.pos = new Float32Array([fp.readFloat(), fp.readFloat()]);
+		this.uv = new Float32Array([
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat()
+		]);
+		this.xy = new Float32Array([
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat(),
+			fp.readFloat()
+		]);
+		this.aniframe = fp.readFloat();
+		this.anitype = fp.readULong();
+		this.delay = fp.readFloat();
+		this.angle = fp.readFloat() / (1024 / 360);
+		this.color = new Float32Array([
+			fp.readFloat() / 255.0,
+			fp.readFloat() / 255.0,
+			fp.readFloat() / 255.0,
+			fp.readFloat() / 255.0
+		]);
+		this.srcalpha = fp.readULong();
+		this.destalpha = fp.readULong();
+		this.mtpreset = fp.readULong();
 	}
 }
-
-/**
- * Parse a STR file
- *
- * @param {ArrayBuffer} data
- */
-STR.prototype.load = function load(data) {
-	let i;
-
-	const fp = new BinaryReader(data);
-	this.header = fp.readBinaryString(4);
-
-	if (this.header !== 'STRM') {
-		throw new Error('STR::load() - Incorrect header "' + this.header + '", must be "STRM"');
-	}
-
-	this.version = fp.readULong();
-
-	if (this.version !== 0x94) {
-		throw new Error('STR - Invalid version "' + this.version + '", not supported');
-	}
-
-	this.fps = fp.readULong();
-	this.maxKey = fp.readULong();
-	this.layernum = fp.readULong();
-	fp.seek(16, SEEK_CUR); // display, group, type, ... ?
-
-	this.layers = new Array(this.layernum);
-
-	for (i = 0; i < this.layernum; ++i) {
-		this.layers[i] = new STRLayer(fp, this.texturePath);
-	}
-};
 
 /**
  * Layer structure
@@ -65,66 +63,72 @@ STR.prototype.load = function load(data) {
  * @param {BinaryReader} fp
  * @param {string} texturePath
  */
-function STRLayer(fp, texturePath) {
-	let i;
+class STRLayer {
+	constructor(fp, texturePath) {
+		let i;
 
-	this.texcnt = fp.readLong();
-	this.texname = new Array(this.texcnt);
+		this.texcnt = fp.readLong();
+		this.texname = new Array(this.texcnt);
 
-	for (i = 0; i < this.texcnt; ++i) {
-		this.texname[i] = 'data\\texture\\effect\\' + texturePath + fp.readBinaryString(128);
-	}
+		for (i = 0; i < this.texcnt; ++i) {
+			this.texname[i] = 'data\\texture\\effect\\' + texturePath + fp.readBinaryString(128);
+		}
 
-	this.anikeynum = fp.readLong();
-	this.animations = new Array(this.anikeynum);
+		this.anikeynum = fp.readLong();
+		this.animations = new Array(this.anikeynum);
 
-	for (i = 0; i < this.anikeynum; ++i) {
-		this.animations[i] = new STRAnimation(fp);
+		for (i = 0; i < this.anikeynum; ++i) {
+			this.animations[i] = new STRAnimation(fp);
+		}
 	}
 }
-
 /**
- * Frame structure
+ * Str class loader
  *
- * @param {BinaryReader} fp
+ * @param {ArrayBuffer} data - optional
+ * @param {string} texturePath - optional
  */
-function STRAnimation(fp) {
-	this.frame = fp.readLong();
-	this.type = fp.readULong();
-	this.pos = new Float32Array([fp.readFloat(), fp.readFloat()]);
-	this.uv = new Float32Array([
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat()
-	]);
-	this.xy = new Float32Array([
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat(),
-		fp.readFloat()
-	]);
-	this.aniframe = fp.readFloat();
-	this.anitype = fp.readULong();
-	this.delay = fp.readFloat();
-	this.angle = fp.readFloat() / (1024 / 360);
-	this.color = new Float32Array([
-		fp.readFloat() / 255.0,
-		fp.readFloat() / 255.0,
-		fp.readFloat() / 255.0,
-		fp.readFloat() / 255.0
-	]);
-	this.srcalpha = fp.readULong();
-	this.destalpha = fp.readULong();
-	this.mtpreset = fp.readULong();
+class STR {
+	constructor(data, texturePath) {
+		this.version = 0.0;
+		this.texturePath = texturePath ?? '';
+
+		if (data) {
+			this.load(data);
+		}
+	}
+	/**
+	 * Parse a STR file
+	 *
+	 * @param {ArrayBuffer} data
+	 */
+	load(data) {
+		let i;
+
+		const fp = new BinaryReader(data);
+		this.header = fp.readBinaryString(4);
+
+		if (this.header !== 'STRM') {
+			throw new Error('STR::load() - Incorrect header "' + this.header + '", must be "STRM"');
+		}
+
+		this.version = fp.readULong();
+
+		if (this.version !== 0x94) {
+			throw new Error('STR - Invalid version "' + this.version + '", not supported');
+		}
+
+		this.fps = fp.readULong();
+		this.maxKey = fp.readULong();
+		this.layernum = fp.readULong();
+		fp.seek(16, SEEK_CUR); // display, group, type, ... ?
+
+		this.layers = new Array(this.layernum);
+
+		for (i = 0; i < this.layernum; ++i) {
+			this.layers[i] = new STRLayer(fp, this.texturePath);
+		}
+	}
 }
 
 /**
