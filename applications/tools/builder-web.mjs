a97f89fc654f0a55eec37083353e14e1198cfcb6
AoShinHo <126742159+AoShinRO@users.noreply.github.com>
2026-04-15 17:59:25 -0300

    fixes icon.png calls, change poring icon for non-copy icon AI made

--- applications/tools/builder-web.mjs
+++ applications/tools/builder-web.mjs
@@ -172,7 +172,7 @@ function createHTML(includeManifest = false) {
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>  
         <meta charset="UTF-8">  
         <title>roBrowser [${pkg.version} - ${buildDate}]</title>  
-        <link rel="icon" type="./icon.png">  
+        <link rel="icon" type="image/png" href="./icon.png">  
   
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">  
         <meta name="HandheldFriendly" content="true">  
