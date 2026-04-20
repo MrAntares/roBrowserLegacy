f7817118b775866da84f3fbbbf58fe1be326f867
AoShinHo <126742159+AoShinRO@users.noreply.github.com>
2026-04-20 15:56:11 -0300

    remove useless https check

--- vite.config.js
+++ vite.config.js
@@ -49,7 +49,6 @@ export default defineConfig({
 	server: {
 		port: 3000,
 		open: true,
-		https: process.env.HTTPS === 'true',
 		proxy: {  
 			'/emblem': {  
 				target: 'http://127.0.0.1:8888',  
